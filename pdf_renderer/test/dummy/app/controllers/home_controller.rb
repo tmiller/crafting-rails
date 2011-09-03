79b992c57b9bb96796de6e1af27c99b2a1f17b76
Tom Miller <jackerran@gmail.com>
2011-09-03 14:27:02 -0500

    Finish learning about creating my own renderer

--- pdf_renderer/test/dummy/app/controllers/home_controller.rb
+++ pdf_renderer/test/dummy/app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ class HomeController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.pdf { render :pdf => "contents" }
+      format.pdf { render :pdf => 'contents' }
     end
+  end
+
+  def another
+    render :pdf => 'contents', :template => 'home/index'
   end
 end
--- pdf_renderer/test/integration/navigation_test.rb
+++ pdf_renderer/test/integration/navigation_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class NavigationTest < ActiveSupport::IntegrationCase
-  test "truth" do
+  test 'truth' do
     assert_kind_of Dummy::Application, Rails.application
   end
 
-  test "pdf request sends a pdf as a file" do
+  test 'pdf request sends a pdf as a file' do
     visit home_path
     click_link "PDF"
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"', headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+  end
 
+  test 'pdf renderer uses the specified template' do
+    visit '/another.pdf'
     assert_equal 'binary', headers['Content-Transfer-Encoding']
     assert_equal 'attachment; filename="contents.pdf"', headers['Content-Disposition']
     assert_equal 'application/pdf', headers['Content-Type']
