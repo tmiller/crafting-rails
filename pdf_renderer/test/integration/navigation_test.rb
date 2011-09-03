d0d2eab7c9d56fbf1eb394acebd26aa7b6a8abba
Tom Miller <jackerran@gmail.com>
2011-09-03 12:08:43 -0500

    Add pdf renderer

--- pdf_renderer/test/integration/navigation_test.rb
+++ pdf_renderer/test/integration/navigation_test.rb
@@ -4,4 +4,17 @@ class NavigationTest < ActiveSupport::IntegrationCase
   test "truth" do
     assert_kind_of Dummy::Application, Rails.application
   end
+
+  test "pdf request sends a pdf as a file" do
+    visit home_path
+    click_link "PDF"
+
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"', headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+  end
+
+  def headers
+    page.response_headers
+  end
 end
