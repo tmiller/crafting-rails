54427efe5bad52cfed660376260e2be5d4c5ff63
Tom Miller <jackerran@gmail.com>
2011-09-03 14:29:20 -0500

    Add newline

--- pdf_renderer/test/test_helper.rb
+++ pdf_renderer/test/test_helper.rb
@@ -18,5 +18,5 @@ Capybara.default_selector = :css
 # Run any available migration
 ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
 
-# Load support files
+# Load support fileup
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
