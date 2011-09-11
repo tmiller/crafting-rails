a098e01155d0e64fee4b7f043814f1be6f3e87ec
Tom Miller <jackerran@gmail.com>
2011-09-11 17:17:19 -0500

    Finish learning about creating my own model

---
+++ mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '542be2758df47b74456046f0724e455df973a5b5f20dd1cf0e83d34d29d1da4688787fc304658f4265436d1e0f7fdd2856f19dc097d5da16adbadf01e251f95f'
